66af14f2a5f76ddd1884cb0042ab90680a61a50f
nabe0316 <nabe0316rt@gmail.com>
2024-07-03 00:15:43 +0900

    ホーム画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'static_pages#top'
+  get 'home', to: 'home#index', as: :home
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
