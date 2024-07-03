88790def32ec8d774c90462db9ae91c9629687c4
nabe0316 <nabe0316rt@gmail.com>
2024-07-04 03:14:44 +0900

    footerを特定のページにのみ表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  helper_method :show_footer?
+
   protected
 
   def after_sign_in_path_for(resource)
@@ -7,5 +9,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     home_path
+  end
+
+  def show_footer?
+    controller_name == 'static_pages' ||
+    (controller_name == 'registrations' && action_name == 'new') ||
+    (controller_name == 'sessions' && action_name == 'new') ||
+    controller_name == 'passwords'
   end
 end
