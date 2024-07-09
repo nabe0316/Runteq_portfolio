5305ec1b40339a707d0a7a1be52929001aafd7a9
nabe0316 <nabe0316rt@gmail.com>
2024-07-09 19:41:22 +0900

    action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,12 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'runteq-portfolio.onrender.com', protocol: 'https' }
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'runteq-portfolio.onrender.com',
+  domain:               'gmail.com',
   user_name:            ENV['SMTP_USERNAME'],
   password:             ENV['SMTP_PASSWORD'],
   authentication:       'plain',
