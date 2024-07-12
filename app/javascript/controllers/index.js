850e0edf9b255acc3a412abd8f50651136385798
nabe0316 <nabe0316rt@gmail.com>
2024-07-13 03:28:39 +0900

    フラッシュメッセージの実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,6 +5,9 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 import LeafHoverController from "./leaf_hover_controller"
 application.register("leaf-hover", LeafHoverController)
+
+import FlashController from "./flash_controller"
+application.register("flash", FlashController)
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 const context = require.context("controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
