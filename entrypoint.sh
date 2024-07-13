5b1a5bd961319c31c2f9de44b06afd81117842bb
nabe0316 <nabe0316rt@gmail.com>
2024-07-14 03:18:49 +0900

    本番環境レイアウト反映

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ rm -f /sample-app/tmp/pids/server.pid
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 bundle install
 yarn install
-yarn build
+yarn build:css
 bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 exec "$@"
