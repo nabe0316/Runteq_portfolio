72598eec2a29bfc1527403536511dbaa51723ff9
nabe0316 <nabe0316rt@gmail.com>
2024-07-14 14:30:18 +0900

    木のレイアウトを変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @tree = current_user.tree || current_user.create_tree!
-      @messages = current_user.messages || []
+      @messages = current_user.messages
       @tree.grow
+      @current_stage = @tree.current_stage
     end
   end
 end
