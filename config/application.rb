14636c63970f0c468f96abb61e288b01a093ec36
nabe0316 <nabe0316rt@gmail.com>
2024-07-15 02:46:45 +0900

    タイムゾーン変更

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module PortfolioApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
