b9d755f13a760695c99f97970ae9e0d72b4f66b4
nabe0316 <nabe0316rt@gmail.com>
2024-08-29 04:20:33 +0900

    検索画面の作成

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,12 @@ class Message < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50}
   validates :content, presence: true, length: { maximum: 255}
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["title", "content", "created_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
